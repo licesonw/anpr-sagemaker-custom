1d973b9524142f80401c569c4d79084c0973c628
EC2 Default User <ec2-user@ip-172-31-6-98.eu-west-1.compute.internal>
2020-12-18 17:46:13 +0000

    fix errors, works now

--- src/predictor.py
+++ src/predictor.py
@@ -5,6 +5,7 @@ from keras.models import model_from_json
 import logging
 import cv2
 import numpy as np
+import utils
 
 MODEL_PATH = '/opt/ml/'
 TMP_MODEL_PATH = '/tmp/ml/model'
@@ -48,10 +49,10 @@ class ClassificationService(object):
         return self.model
     
     def predict(self, img):
-        img = self._preprocess_img(img)
+        img = self._preprocess(img)
         return self.model.predict(img)
         
-    def _preprocess_img(self, img):
+    def _preprocess(self, img):
         max_dim = 608
         min_dim_img = min(img.shape[:2])
         factor = float(max_dim) / min_dim_img
@@ -60,6 +61,23 @@ class ClassificationService(object):
         img_cpy = img_resized.copy()
         img_cpy = img_cpy.reshape((1, img_cpy.shape[0], img_cpy.shape[1], img_cpy.shape[2]))
         return img_cpy
+        
+    def predict_and_reconstruct(self, img):
+        """Process according to Github repo"""
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img / 255
+        
+        max_dim = 608
+        min_dim_img = min(img.shape[:2])
+        factor = float(max_dim) / min_dim_img
+        w, h = (np.array(img.shape[1::-1], dtype=float) * factor).astype(int).tolist()
+        img_resized = cv2.resize(img, (w, h))
+        img_cpy = img_resized.copy()
+        img_cpy = img_cpy.reshape((1, img_cpy.shape[0], img_cpy.shape[1], img_cpy.shape[2]))
+        Y = self.model.predict(img_cpy)
+        Y = np.squeeze(Y)
+        L, TLp, lp_type, Cor = utils.reconstruct(img, img_resized, Y, 0.5)
+        return L, TLp, lp_type, Cor
 
 #Define the path
 model_path = os.path.join(MODEL_PATH, 'model')
@@ -86,11 +104,12 @@ def transformation():
     
     write_test_image(flask.request.stream)
     img = cv2.imread(IMG_FOR_INFERENCE)
-    predictions = clf.predict(img)
+    L, TLp, lp_type, coors = clf.predict_and_reconstruct(img)
 
     # Transform predictions to JSON
     result = {
-        'output': predictions.tolist()
+        'TLp': (np.array(TLp)).tolist(),
+        'coors': (np.array(coors)).tolist()
         }
 
     resultjson = json.dumps(result)
--- test/test.py
+++ test/test.py
@@ -1,9 +1,13 @@
 import os
 import json
 import requests
+import numpy as np
+from imageio import imwrite
 
+IMAGE = 'germany_car_plate.jpg'
 payload = ''
-with open('img.jpg', 'rb') as f:
+
+with open(IMAGE, 'rb') as f:
     payload = f.read()
     payload = bytearray(payload)
     
@@ -12,4 +16,13 @@ URL = 'http://127.0.0.1:8080/invocations'
 headers = {'Content-type': 'application/x-image'}
 resp = requests.post(URL, data=payload, headers=headers)
 
-print(resp.content)
+
+#print(resp.content)
+data = json.loads(resp.content)
+cropped_img = np.array(data['TLp'])
+cropped_img = cropped_img[0]
+coords = np.array(data['coors'])
+
+print(cropped_img.shape)
+
+imwrite('inference.jpg', cropped_img)
