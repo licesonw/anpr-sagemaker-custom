a8cc272027aa89a4f2580b3b2f7fb74d87b867eb
EC2 Default User <ec2-user@ip-172-31-6-98.eu-west-1.compute.internal>
2020-12-19 13:12:52 +0000

    remove script

--- build_push_ecr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Builds Docker image and pushes to ECR repo
-
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 076415213549.dkr.ecr.eu-west-1.amazonaws.com
-docker build -t anpr-custom-inference .
-docker tag anpr-custom-inference:latest 076415213549.dkr.ecr.eu-west-1.amazonaws.com/anpr-custom-inference:latest
-docker push 076415213549.dkr.ecr.eu-west-1.amazonaws.com/anpr-custom-inference:latest
